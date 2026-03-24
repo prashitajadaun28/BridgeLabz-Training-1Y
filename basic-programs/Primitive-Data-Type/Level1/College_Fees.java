761894ce2078d8c4bda6c639b1b5dadc180e2760
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 01:42:27 +0530

    Delete basic-programs directory

--- basic-programs/Primitive-Data-Type/Level1/College_Fees.java
+++
@@ -1,8 +0,0 @@
-class College_Fees{
-	public static void main(String args[]){
-		int fees=125000;
-		int discount=(125000/100)*10;
-		int final_fee=fees-discount;
-		System.out.print("The discount amount is INR "+discount+" and final discounted fee is INR "+final_fee);
-	}
-}
--- basic-programs/Primitive-Data-Type/Level1/Convert_Km_To_Miles_Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Convert_Km_To_Miles_Input{
-	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
-		float km=sc.nextFloat();
-		float mile=km/1.6f;
-		System.out.print("The total miles is "+mile+" mile for the given "+km+" km");
-	}
-}
