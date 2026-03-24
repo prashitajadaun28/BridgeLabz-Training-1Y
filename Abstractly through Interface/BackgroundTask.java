68e7e3455e3c963f81a75d8e924edba36c766b04
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 01:44:11 +0530

    Delete Abstractly through Interface directory

--- Abstractly through Interface/BackgroundTask.java
+++
@@ -1,6 +0,0 @@
-public class BackgroundTask {
-    public static void main(String[] args) {
-        Runnable task = () -> System.out.println("Task Running");
-        new Thread(task).start();
-    }
-}
