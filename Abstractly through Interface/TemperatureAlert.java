c893bc7c2a0b3191faf5fb0d51446a7806a8b2c4
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 01:40:31 +0530

    Delete Abstractly through Interface directory

--- Abstractly through Interface/TemperatureAlert.java
+++
@@ -1,8 +0,0 @@
-import java.util.function.Predicate;
-
-public class TemperatureAlert {
-    public static void main(String[] args) {
-        Predicate<Double> alert = temp -> temp > 40;
-        System.out.println(alert.test(45.0));
-    }
-}
