832834b2c5146769967c0ed22c8b2c473e26aa44
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 01:25:19 +0530

    Delete basic-programs directory

--- basic-programs/Primitive-Data-Type/Level1/Sams_Average_Marks.java
+++
@@ -1,9 +0,0 @@
-class Sams_Average_Marks{
-	public static void main(String args[]){
-		float maths=94;
-		float physics=95;
-		float chemistry=96;
-		float average=(maths+physics+chemistry)/3;
-		System.out.print("Sam’s average mark in PCM is "+average);
-	}
-}
