93b89d9d31b07b85ab6c01c4e329288c0115c791
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 01:20:32 +0530

    Delete basic-programs directory

--- basic-programs/Primitive-Data-Type/Level1/Side_Of_Square.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Side_Of_Square{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        float perimeter=sc.nextFloat();
-        float side=perimeter/4;
-        System.out.print("The length of the side is " +side+" whose perimeter is "+perimeter);
-    }
-}
