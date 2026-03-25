9d6fff1fe254eaf6d350253bf9cd0b6b6c4406de
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 09:27:32 +0530

    Delete basic-programs directory

--- basic-programs/Primitive-Data-Type/Level1/Maximum_Handshakes.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Maximum_Handshakes {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int numberOfStudents=sc.nextInt();
-        int handshakes=(numberOfStudents*(numberOfStudents-1))/2;
-        System.out.print("The maximum number of handshakes among "+numberOfStudents+" students is "+handshakes);
-    }
-}
