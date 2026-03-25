9a2a1632c520eafeb690674e7edb320d4894ee36
prashitajadaun28 <prashitajadaun00@gmail.com>
2026-03-25 09:25:39 +0530

    Delete Abstractly through Interface directory

--- Abstractly through Interface/StringChecker.java
+++
@@ -1,8 +0,0 @@
-import java.util.function.Function;
-
-public class StringChecker {
-    public static void main(String[] args) {
-        Function<String, Integer> length = str -> str.length();
-        System.out.println(length.apply("Hello"));
-    }
-}
