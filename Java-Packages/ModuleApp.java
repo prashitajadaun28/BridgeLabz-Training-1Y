c81bd7094ec2fc6b67b153f4e90cb3d3554eb30d
Ansh Verma <anshverma151106@gmail.com>
2026-04-17 15:22:03 +0530

    Delete Java-Packages directory

--- Java-Packages/ModuleApp.java
+++
@@ -1,8 +0,0 @@
-import college.student.Student;
-
-public class ModuleApp {
-    public static void main(String[] args) {
-        Student s = new Student();
-        s.display();
-    }
-}
--- Java-Packages/UniversityApp.java
+++
@@ -1,8 +0,0 @@
-import com.university.department.cse.Course;
-
-public class UniversityApp {
-    public static void main(String[] args) {
-        Course c = new Course();
-        c.display();
-    }
-}
